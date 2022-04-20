f00c509afb42b01215aaee8d9d0804911a5d1e10
Marius <marius@example.com>
2022-04-20 15:03:18 +0300

    eslint rm, userArr

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,34 @@
 "use strict";
 console.log('main.ts');
+const obj1 = {
+    name: 'James',
+    age: 30,
+    greet: () => {
+        console.log(`${this.name} is ${this.age} year old. Hi!`);
+    },
+};
+const obj2 = {
+    name: 'Mike',
+    age: 50,
+    greet: () => {
+        console.log(`${this.name} is ${this.age} year old. Hi!`);
+    },
+};
+console.log('obj1-2 ===', obj1, obj2);
+class UserObj {
+    constructor(n, a) {
+        this.name = n;
+        this.age = a;
+    }
+    greet() {
+        console.log(`${this.name} is ${this.age} year old. Hi!`);
+    }
+}
+const obj3 = new UserObj('Jill', 35);
+obj3.greet();
+const obj4 = new UserObj('Bob', 45);
+console.log('obj4 ===', obj4);
+const usersArr = [obj3];
+usersArr.push({ name: 'james', age: 25 });
+usersArr.push(obj2);
+console.log('usersArr ===', usersArr);
--- ts/main.ts
+++ ts/main.ts
@@ -5,3 +5,53 @@ console.log('main.ts');
 
 // nustatyti kad naudosim es6 modules
 // "module": "es2015",
+
+// OBJ
+
+type User = {
+  name: string;
+  age: number;
+  greet: () => void;
+};
+
+const obj1: User = {
+  name: 'James',
+  age: 30,
+  greet: () => {
+    console.log(`${this.name} is ${this.age} year old. Hi!`);
+  },
+};
+const obj2: User = {
+  name: 'Mike',
+  age: 50,
+  greet: () => {
+    console.log(`${this.name} is ${this.age} year old. Hi!`);
+  },
+};
+console.log('obj1-2 ===', obj1, obj2);
+
+class UserObj {
+  name: string;
+
+  age: number;
+
+  constructor(n: string, a: number) {
+    this.name = n;
+    this.age = a;
+  }
+
+  greet() {
+    console.log(`${this.name} is ${this.age} year old. Hi!`);
+  }
+}
+
+const obj3 = new UserObj('Jill', 35);
+obj3.greet();
+const obj4 = new UserObj('Bob', 45);
+console.log('obj4 ===', obj4);
+
+const usersArr: UserObj[] = [obj3];
+usersArr.push({ name: 'james', age: 25 }); // err
+usersArr.push(obj2);
+
+console.log('usersArr ===', usersArr);
